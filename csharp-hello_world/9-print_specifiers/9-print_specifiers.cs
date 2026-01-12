c0141a80ab5a133d4e27fd615d38784936907335
gusenga thierry <t.gusenga@alustudent.com>
2026-01-12 09:47:21 +0200

    c sharp

---
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		Console.WriteLine($"Percent: {percent.ToString("P2", System.Globalization.CultureInfo.CreateSpecificCulture("en-US"))}\nCurrency: {currency.ToString("C", System.Globalization.CultureInfo.CreateSpecificCulture("en-US"))}");
+	}
+}
