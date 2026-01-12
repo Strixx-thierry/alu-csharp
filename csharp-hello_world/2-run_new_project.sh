892019b49e836d5562d4b7b21ca936908a48356c
gusenga thierry <t.gusenga@alustudent.com>
2026-01-12 10:09:19 +0200

    force

--- csharp-hello_world/2-run_new_project.sh
+++ csharp-hello_world/2-run_new_project.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
-dotnet new console -o 2-new_project
+dotnet new console -o 2-new_project --force
+echo 'using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}' > 2-new_project/Program.cs
 dotnet build 2-new_project
 dotnet run --project 2-new_project
