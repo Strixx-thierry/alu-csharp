3721a82744f28d2105df28a469398415dd8a4022
gusenga thierry <t.gusenga@alustudent.com>
2026-01-12 10:15:37 +0200

    Update 2-run_new_project.sh

--- csharp-hello_world/2-run_new_project.sh
+++ csharp-hello_world/2-run_new_project.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
-dotnet new console -o 2-new_project --force
-echo 'using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World!");
-    }
-}' > 2-new_project/Program.cs
-dotnet build 2-new_project
-dotnet run --project 2-new_project
+dotnet new console -n 1-new_project
+cd 1-new_project
+dotnet build
