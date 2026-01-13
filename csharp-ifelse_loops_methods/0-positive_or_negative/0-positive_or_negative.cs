224ba8075bb286581c3ae7317bccf493caea59a4
gusenga thierry <t.gusenga@alustudent.com>
2026-01-13 16:30:35 +0200

    last

---
+++ csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        int number = rnd.Next(-10, 11);
+        if (number > 0)
+        {
+            Console.WriteLine(number + " is positive");
+        }
+        else if (number == 0)
+        {
+            Console.WriteLine(number + " is zero");
+        }
+        else
+        {
+            Console.WriteLine(number + " is negative");
+        }
+    }
+}
