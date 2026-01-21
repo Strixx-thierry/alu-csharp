edf9668f52f761d0a1501581d2a86a2ef33b2c91
gusenga thierry <t.gusenga@alustudent.com>
2026-01-21 13:44:32 +0200

    charp array

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -2,12 +2,7 @@ using System;
 
 class Array
 {
-    /// <summary>
-    /// Creates and prints an array of integers of a given size.
-    /// </summary>
-    /// <param name="size">The size of the array.</param>
-    /// <returns>The new array, or null if size is negative.</returns>
-    public static int[] CreatePrint(int size)
+    public static int?[] CreatePrint(int size)
     {
         if (size < 0)
         {
@@ -15,19 +10,23 @@ class Array
             return null;
         }
 
-        int[] newArray = new int[size];
+        int?[] arr = new int?[size];
+
+        if (size == 0)
+        {
+            Console.WriteLine();
+            return arr;
+        }
 
         for (int i = 0; i < size; i++)
         {
-            newArray[i] = i;
-            Console.Write(newArray[i]);
+            arr[i] = i;
+            Console.Write(i);
             if (i < size - 1)
-            {
                 Console.Write(" ");
-            }
         }
 
         Console.WriteLine();
-        return newArray;
+        return arr;
     }
 }
