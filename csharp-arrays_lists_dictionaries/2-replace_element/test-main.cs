83324041a7c2f173b55a66c0e39896649e7e0b4b
gusenga thierry <t.gusenga@alustudent.com>
2026-01-22 12:55:53 +0200

    2-replace_element

---
+++ csharp-arrays_lists_dictionaries/2-replace_element/test-main.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = {0, 1, 2, 3, 4, 5, 6};
+        PrintArray(array);
+        Array.ReplaceElement(array, 3, 98);
+        PrintArray(array);
+    }
+
+    static void PrintArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i]);
+            if (i < array.Length - 1)
+                Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+}
