03e3f605019f2b21300fd0bff092e0dcf7b68d6e
gusenga thierry <t.gusenga@alustudent.com>
2026-01-26 12:20:10 +0200

    1,2

--- csharp-hello_world/2-run_new_project.sh
+++ csharp-hello_world/2-run_new_project.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
-
-dotnet new console -o 2-new_project
-
-dotnet build 2-new_project/2-new_project.csproj
-
-dotnet run --project 2-new_project/2-new_project.csproj
+dotnet new console -o 2-new_project --force
+dotnet run --project 2-new_project
