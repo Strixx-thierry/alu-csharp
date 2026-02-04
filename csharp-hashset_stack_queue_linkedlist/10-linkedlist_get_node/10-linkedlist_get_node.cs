68558cc8ff4a3af88829f96d239c1af001d2ebba
gusenga thierry <t.gusenga@alustudent.com>
2026-02-04 10:11:23 +0200

    10-linkedlist

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        if (n < 0)
+        {
+            return 0;
+        }
+
+        int index = 0;
+        foreach (int item in myLList)
+        {
+            if (index == n)
+            {
+                return item;
+            }
+            index++;
+        }
+        return 0;
+    }
+}
