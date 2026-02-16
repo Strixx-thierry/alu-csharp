dbf6e5d0c27efd13f459980a9a8184196b3e33fc
gusenga thierry <t.gusenga@alustudent.com>
2026-02-16 11:04:12 +0200

    1-divide_print

---
+++ csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        int result = 0;
+        try
+        {
+            result = a / b;
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+            result = 0;
+        }
+        finally
+        {
+            Console.WriteLine(a + " / " + b + " = " + result);
+        }
+    }
+}
