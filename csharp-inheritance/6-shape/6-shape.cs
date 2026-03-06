1f48720ba2ddcfcbde9124ad3dc01727409725c6
gusenga thierry <t.gusenga@alustudent.com>
2026-03-06 04:56:56 +0200

    6 shape

---
+++ csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+/// <summary>
+/// This class represents a base Shape.
+/// </summary>
+public class Shape
+{
+    /// <summary>
+    /// Calculates the area of the shape.
+    /// </summary>
+    /// <returns>None. Throws NotImplementedException.</returns>
+    /// <exception cref="NotImplementedException">Thrown always with message Area() is not implemented.</exception>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+/// <summary>
+/// This class represents a Rectangle, inheriting from Shape.
+/// </summary>
+public class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    /// <summary>
+    /// Gets or sets the width of the rectangle.
+    /// </summary>
+    /// <exception cref="ArgumentException">Thrown when value is negative.</exception>
+    public int Width
+    {
+        get { return width; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            width = value;
+        }
+    }
+
+    /// <summary>
+    /// Gets or sets the height of the rectangle.
+    /// </summary>
+    /// <exception cref="ArgumentException">Thrown when value is negative.</exception>
+    public int Height
+    {
+        get { return height; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            height = value;
+        }
+    }
+}
