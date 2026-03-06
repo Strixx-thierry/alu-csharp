0e7c196fd370ced481a1fc11dfcc8125d5c1d911
gusenga thierry <t.gusenga@alustudent.com>
2026-03-06 04:53:23 +0200

    4 inherit

---
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary>
+/// This is an empty base class.
+/// </summary>
+public class Animal
+{
+}
+
+/// <summary>
+/// This class inherits from Animal.
+/// </summary>
+public class Dog : Animal
+{
+}
