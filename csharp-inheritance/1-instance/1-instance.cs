acc9ac5f8747b4a8b0c9d8c046e6e86053793324
gusenga thierry <t.gusenga@alustudent.com>
2026-03-06 04:50:10 +0200

    1

---
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// This class contains methods for object type checking.
+/// </summary>
+public class Obj
+{
+    /// <summary>
+    /// Checks if an object is an instance of Array or inherits from Array.
+    /// </summary>
+    /// <param name="obj">The object to check.</param>
+    /// <returns>True if the object is an instance of Array or a subclass of it, otherwise False.</returns>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return obj is Array;
+    }
+}
