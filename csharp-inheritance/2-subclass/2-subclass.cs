287851aa9a95aa6ec104c30b5fbe7e7e839b7a21
gusenga thierry <t.gusenga@alustudent.com>
2026-03-06 04:50:33 +0200

    2 sub

---
+++ csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/// <summary>
+/// This class contains methods for object type checking.
+/// </summary>
+public class Obj
+{
+    /// <summary>
+    /// Checks if a type is a subclass of another type, excluding the base type itself.
+    /// </summary>
+    /// <param name="derivedType">The type to check.</param>
+    /// <param name="baseType">The base type.</param>
+    /// <returns>True if derivedType is a subclass of baseType, False otherwise.</returns>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        return derivedType.IsSubclassOf(baseType);
+    }
+}
