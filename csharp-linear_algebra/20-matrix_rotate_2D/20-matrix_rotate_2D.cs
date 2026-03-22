b1ec770aeef2e0b3afa823b5b3ba329c4860bc24
gusenga thierry <t.gusenga@alustudent.com>
2026-03-22 14:18:49 +0200

    ok

--- csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -13,10 +13,7 @@ public class MatrixMath
     /// <returns>Resulting matrix, or [-1] if invalid.</returns>
     public static double[,] Rotate2D(double[,] matrix, double angle)
     {
-        int rows = matrix.GetLength(0);
-        int cols = matrix.GetLength(1);
-
-        if (rows != 2 || cols != 2)
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
         {
             return new double[,] { { -1 } };
         }
@@ -30,10 +27,11 @@ public class MatrixMath
             double x = matrix[i, 0];
             double y = matrix[i, 1];
             
-            result[i, 0] = x * cosA - y * sinA;
-            result[i, 1] = x * sinA + y * cosA;
+            result[i, 0] = Math.Round(x * cosA - y * sinA, 2);
+            result[i, 1] = Math.Round(x * sinA + y * cosA, 2);
         }
 
         return result;
     }
 }
+
